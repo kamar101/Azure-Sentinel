b9fca978127aae1ea5a305d00130e4a989f1cc63
Samuel <kamarsamuel9@gmail.com>
2022-08-26 16:07:51 -0400

    "Added Error Handling and Comments to script"

--- scripts.ps1
+++ scripts.ps1
@@ -1,16 +1,19 @@
-﻿$key = "access_key=d99dc4c48498db0c8698bcd2ced300f8"
+﻿$key = "access_key=4cbbed61a1d2aa72a98da7097f8178a9"
 $API_URl = "http://api.ipapi.com/"
-$LOGFILE_PATH = "C:\Users\OneUser\Documents\GeneratedLogs\logs.txt"
+$LOGFILE_PATH = "C:\Users\mrhacker\Documents\logs.txt"
 
 $events = Get-EventLog -LogName Security | Where-Object {$_.EventID -eq 4625}
 
 foreach($log in $events){
+
     $timestap = $log.TimeWritten
     $user_name = $log.ReplacementStrings[5]
     $computer_name = $log.ReplacementStrings[1]
     $event_ip = $log.ReplacementStrings[19]
 
     if ((Test-Path $LOGFILE_PATH) -eq $false) {
+        
+        #Create Log file with sample logs
         New-Item -ItemType File -Path $LOGFILE_PATH
         "latitude:47.91542,longitude:-120.60306,destinationhost:samplehost,username:fakeuser,sourcehost:24.16.97.222,state:Washington,country:United States,label:United States - 24.16.97.222,timestamp:2021-10-26 03:28:29" | Out-File $LOGFILE_PATH -Append -Encoding utf8
         "latitude:-22.90906,longitude:-47.06455,destinationhost:samplehost,username:lnwbaq,sourcehost:20.195.228.49,state:Sao Paulo,country:Brazil,label:Brazil - 20.195.228.49,timestamp:2021-10-26 05:46:20" | Out-File $LOGFILE_PATH -Append -Encoding utf8
@@ -20,12 +23,23 @@ foreach($log in $events){
   
      }
 
+    #Used to prevent duplicates
     $file = Get-Content $LOGFILE_PATH
     $containsWord = $file | %{$_ -match "timestamp:$timestap"}
 
     if($containsWord -contains $false){
+
+        #Sending API request
         $complete_URL = $API_URl + $event_ip + "?"+$key
         $request_response = Invoke-RestMethod $complete_URL
+
+        # Error Handling for failed API request
+        if($request_response.success -eq $False){
+            echo $request_response
+            break
+        }
+
+        #Phrasing API output
         $ip = $request_response.ip
         $type =$request_response.type          
         $country_code   = $request_response.country_code
@@ -36,15 +50,11 @@ foreach($log in $events){
         $latitude = $request_response.latitude
         $longitude =$request_response.longitude
 
+        #Formating logs for entry
         $entry = "latitude:$latitude,longitude:$longitude,username:$user_name,sourcehost:$event_ip,city:$city,country:$country_name,label:$country_name - $ip,timestamp:$timestap"
+
+        #Writing logs to file
         echo "writting:  $entry "
         $entry |Out-File $LOGFILE_PATH -Append -Encoding utf8 
 
 }}
-
-
-#you would like to add everthing to functions
-#you think it will be nice if request is only made to the api if timestap is unique in the log lfile.
-
-
-
